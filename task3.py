8f0c3e838939be0968bbd8c27e10142708434294
JIdigit <emirbek.b@gmail.com>
2019-11-18 19:56:59 +0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -4,11 +4,15 @@ for i in range(0,5):
 print('Finish of the input')
 shift_num = int(input())
 
-while shift_num >= 0:
+while shift_num > 0:
     temp_value = my_list.pop()
     my_list.insert(0,temp_value)
     shift_num-=1
-
+else:
+    while shift_num <0: 
+        temp_value = my_list.pop(0)
+        my_list.append(temp_value)
+        shift_num+=1
 print (my_list)
 
 
